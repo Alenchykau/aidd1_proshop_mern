bf09bf2e6396ad4e38c1a34a735588f0f0ac212d
alenchykau <l3nchi3@gmail.com>
2026-05-10 14:42:22 +0300

    course: feat: add optional cta slot to EmptyState

--- frontend/src/components/EmptyState.js
+++ frontend/src/components/EmptyState.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './EmptyState.css'
 
-const EmptyState = ({ icon, heading, subtitle }) => {
+const EmptyState = ({ icon, heading, subtitle, cta }) => {
   return (
     <div className='empty-state'>
       {icon ? <div className='empty-state-icon'>{icon}</div> : null}
       <h3>{heading}</h3>
-      <p>{subtitle}</p>
+      {subtitle ? <p>{subtitle}</p> : null}
+      {cta ? <div className='empty-state-cta'>{cta}</div> : null}
     </div>
   )
 }
