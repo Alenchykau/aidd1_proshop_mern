86dbb1db1e20bf33432b35add48512f90f2a10ca
alenchykau <l3nchi3@gmail.com>
2026-05-10 14:44:44 +0300

    course: refactor: token-driven hero carousel and drop legacy CSS

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -5,10 +5,10 @@ import { useDispatch, useSelector } from 'react-redux'
 import Loader from './Loader'
 import Message from './Message'
 import { listTopProducts } from '../actions/productActions'
+import './ProductCarousel.css'
 
 const ProductCarousel = () => {
   const dispatch = useDispatch()
-
   const productTopRated = useSelector((state) => state.productTopRated)
   const { loading, error, products } = productTopRated
 
@@ -16,20 +16,18 @@ const ProductCarousel = () => {
     dispatch(listTopProducts())
   }, [dispatch])
 
-  return loading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant='danger'>{error}</Message>
-  ) : (
-    <Carousel pause='hover' className='bg-dark'>
+  if (loading) return <Loader />
+  if (error) return <Message variant='danger'>{error}</Message>
+
+  return (
+    <Carousel pause='hover' className='product-carousel'>
       {products.map((product) => (
         <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className='carousel-caption'>
-              <h2>
-                {product.name} (${product.price})
-              </h2>
+          <Link to={`/product/${product._id}`} className='product-carousel__link'>
+            <Image src={product.image} alt={product.name} fluid className='product-carousel__image' />
+            <Carousel.Caption className='product-carousel__caption'>
+              <h2 className='product-carousel__name'>{product.name}</h2>
+              <span className='product-carousel__price'>${product.price}</span>
             </Carousel.Caption>
           </Link>
         </Carousel.Item>
