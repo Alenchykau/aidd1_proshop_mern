323b3317c36dc71a2a544e0a5c8d7ae916863d8e
alenchykau <l3nchi3@gmail.com>
2026-05-10 16:27:34 +0300

    course: feat: redesign LoginScreen with FormCard atoms

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
-import Loader from '../components/Loader'
-import FormContainer from '../components/FormContainer'
+import FormCard from '../components/ui/FormCard'
+import FormField from '../components/ui/FormField'
+import Button from '../components/ui/Button'
 import { login } from '../actions/userActions'
+import './auth-form.css'
 
 const LoginScreen = ({ location, history }) => {
   const [email, setEmail] = useState('')
@@ -30,45 +31,39 @@ const LoginScreen = ({ location, history }) => {
   }
 
   return (
-    <FormContainer>
-      <h1>Sign In</h1>
+    <FormCard title='Sign In'>
       {error && <Message variant='danger'>{error}</Message>}
-      {loading && <Loader />}
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId='email'>
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Enter email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-
-        <Form.Group controlId='password'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Enter password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-
-        <Button type='submit' variant='primary'>
+      <form onSubmit={submitHandler}>
+        <FormField
+          id='email'
+          label='Email Address'
+          type='email'
+          placeholder='Enter email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <FormField
+          id='password'
+          label='Password'
+          type='password'
+          placeholder='Enter password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <Button type='submit' variant='primary' loading={loading}>
           Sign In
         </Button>
-      </Form>
+      </form>
 
-      <Row className='py-3'>
-        <Col>
-          New Customer?{' '}
-          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-            Register
-          </Link>
-        </Col>
-      </Row>
-    </FormContainer>
+      <p className='auth-form__footer'>
+        New Customer?{' '}
+        <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+          Register
+        </Link>
+      </p>
+    </FormCard>
   )
 }
 
