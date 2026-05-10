aa0246e27d1ab530f63fbe9f3c5d5acc60691854
alenchykau <l3nchi3@gmail.com>
2026-05-10 14:51:39 +0300

    course: feat: redesign CartScreen with 2-col layout and EmptyState

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
-import Message from '../components/Message'
+import Card from '../components/ui/Card'
+import Button from '../components/ui/Button'
+import EmptyState from '../components/EmptyState'
 import { addToCart, removeFromCart } from '../actions/cartActions'
+import './CartScreen.css'
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id
-
   const qty = location.search ? Number(location.search.split('=')[1]) : 1
 
   const dispatch = useDispatch()
@@ -29,85 +30,73 @@ const CartScreen = ({ match, location, history }) => {
     history.push('/login?redirect=shipping')
   }
 
+  const itemCount = cartItems.reduce((acc, item) => acc + item.qty, 0)
+  const subtotal = cartItems.reduce((acc, item) => acc + item.qty * item.price, 0)
+
+  if (cartItems.length === 0) {
+    return (
+      <div style={{ padding: 'var(--space-2xl) 0' }}>
+        <EmptyState
+          heading='Your cart is empty'
+          subtitle='Browse the catalogue to add products'
+          cta={<Link to='/' className='ui-btn ui-btn--primary'>Browse Catalogue</Link>}
+        />
+      </div>
+    )
+  }
+
   return (
-    <Row>
-      <Col md={8}>
-        <h1>Shopping Cart</h1>
-        {cartItems.length === 0 ? (
-          <Message>
-            Your cart is empty <Link to='/'>Go Back</Link>
-          </Message>
-        ) : (
-          <ListGroup variant='flush'>
-            {cartItems.map((item) => (
-              <ListGroup.Item key={item.product}>
-                <Row>
-                  <Col md={2}>
-                    <Image src={item.image} alt={item.name} fluid rounded />
-                  </Col>
-                  <Col md={3}>
-                    <Link to={`/product/${item.product}`}>{item.name}</Link>
-                  </Col>
-                  <Col md={2}>${item.price}</Col>
-                  <Col md={2}>
-                    <Form.Control
-                      as='select'
-                      value={item.qty}
-                      onChange={(e) =>
-                        dispatch(
-                          addToCart(item.product, Number(e.target.value))
-                        )
-                      }
-                    >
-                      {[...Array(item.countInStock).keys()].map((x) => (
-                        <option key={x + 1} value={x + 1}>
-                          {x + 1}
-                        </option>
-                      ))}
-                    </Form.Control>
-                  </Col>
-                  <Col md={2}>
-                    <Button
-                      type='button'
-                      variant='light'
-                      onClick={() => removeFromCartHandler(item.product)}
-                    >
-                      <i className='fas fa-trash'></i>
-                    </Button>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
-        )}
-      </Col>
-      <Col md={4}>
-        <Card>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <h2>
-                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
-                items
-              </h2>
-              $
-              {cartItems
-                .reduce((acc, item) => acc + item.qty * item.price, 0)
-                .toFixed(2)}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Button
-                type='button'
-                className='btn-block'
-                disabled={cartItems.length === 0}
-                onClick={checkoutHandler}
+    <div className='cart-page'>
+      <div>
+        <h1 className='cart-page__title'>Shopping Cart</h1>
+        <div className='cart-list'>
+          {cartItems.map((item) => (
+            <div className='cart-row' key={item.product}>
+              <img src={item.image} alt={item.name} className='cart-row__image' />
+              <div className='cart-row__name'>
+                <Link to={`/product/${item.product}`}>{item.name}</Link>
+              </div>
+              <select
+                className='cart-row__qty'
+                value={item.qty}
+                onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
+                aria-label={`Quantity for ${item.name}`}
               >
-                Proceed To Checkout
-              </Button>
-            </ListGroup.Item>
-          </ListGroup>
-        </Card>
-      </Col>
-    </Row>
+                {[...Array(item.countInStock).keys()].map((x) => (
+                  <option key={x + 1} value={x + 1}>{x + 1}</option>
+                ))}
+              </select>
+              <div className='cart-row__price'>${item.price}</div>
+              <button
+                type='button'
+                className='ui-btn ui-btn--icon ui-btn--ghost cart-row__remove'
+                onClick={() => removeFromCartHandler(item.product)}
+                aria-label={`Remove ${item.name}`}
+              >
+                <i className='fas fa-trash' aria-hidden='true' />
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <Card className='cart-summary'>
+        <div className='cart-summary__row'>
+          <span>Subtotal ({itemCount} items)</span>
+        </div>
+        <div className='cart-summary__row'>
+          <span>Total</span>
+          <span className='cart-summary__total'>${subtotal.toFixed(2)}</span>
+        </div>
+        <Button
+          variant='primary'
+          onClick={checkoutHandler}
+          className='cart-summary__action'
+        >
+          Proceed To Checkout
+        </Button>
+      </Card>
+    </div>
   )
 }
 
