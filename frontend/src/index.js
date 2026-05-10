e26e2f3ab6565c114131a85fe6cce986d59153ee
alenchykau <l3nchi3@gmail.com>
2026-05-10 13:06:49 +0300

    course: feat: wrap app in ThemeProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,16 +6,16 @@ import './bootstrap.min.css'
 import './bootstrap-overrides.css'
 import './index.css'
 import App from './App'
+import { ThemeProvider } from './context/ThemeContext'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
