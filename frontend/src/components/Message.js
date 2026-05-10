44c518ba746da894c85f7af743ef4106d1fe47a9
alenchykau <l3nchi3@gmail.com>
2026-05-10 14:41:05 +0300

    course: refactor: token-driven Message alert variants

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Alert } from 'react-bootstrap'
+import './Message.css'
 
-const Message = ({ variant, children }) => {
-  return <Alert variant={variant}>{children}</Alert>
-}
+const Message = ({ variant, children }) => (
+  <Alert variant={variant} className='app-message'>{children}</Alert>
+)
 
 Message.defaultProps = {
   variant: 'info',
